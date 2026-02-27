173ad25f05f9ed84cd3c8835c41bcce7096c9d59
2rangs <2_rang25@naver.com>
2026-02-27 17:44:43 +0900

    fix:  경로 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,15 @@ import { createBrowserRouter } from 'react-router-dom'
 import Pokedex from '../page/pokedex'
 import PokemonDetailPage from '../page/PokemonDetailPage'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+    [
+        { path: '/', element: <Pokedex /> },
+        { path: '/pokemon/:name', element: <PokemonDetailPage /> },
+    ],
     {
-        path: '/',
-        element: <Pokedex />,
-    },
-    {
-        path: '/pokemon/:name',
-        element: <PokemonDetailPage />,
-    },
-])
+        // GitHub Pages에서 /<repo>/ 아래로 라우팅되게 함
+        basename: import.meta.env.BASE_URL,
+    }
+)
 
 export default router
